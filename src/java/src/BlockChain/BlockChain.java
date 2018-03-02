abafbaaad01e814f4f945728db2902b80d0c4238
Banaah <nouvel.quentin@gmail.com>
2018-03-02 23:09:48 +0100

    Ajout du gson.jar

--- src/java/src/BlockChain/BlockChain.java
+++ src/java/src/BlockChain/BlockChain.java
@@ -3,28 +3,32 @@ package BlockChain;
 import java.util.ArrayList;
 
 public class BlockChain {
-    private int difficulte;
+    private int difficulty;
     private int nbBlocks;
-    private ArrayList<Block> BlockChain;
+    private ArrayList<Block> BC;
 
-    public BlockChain(int difficulte) {
-        this.difficulte = difficulte;
+    public BlockChain(int difficulty) {
+        this.difficulty = difficulty;
         this.nbBlocks = 0;
-        BlockChain = new ArrayList<Block>();
+        BC = new ArrayList<Block>();
         String transactions[] = new String[1];
         transactions[0] = "genesis block";
-        BlockChain.add(new Block(this.nbBlocks++, "0", 1, transactions, this.difficulte));
+        BC.add(new Block(this.nbBlocks++, "0", 1, transactions, this.difficulty));
     }
 
-    public int getDifficulte() {
-        return difficulte;
+    void addBlock(String transactions[]){
+        BC.add( new Block(this.nbBlocks++, BC.get(BC.size()-1).getBlockHash(), transactions.length, transactions, this.difficulty));
+    }
+
+    public int getDifficulty() {
+        return difficulty;
     }
 
     public int getNbBlocks() {
         return nbBlocks;
     }
 
-    public ArrayList<Block> getBlockChain() {
-        return BlockChain;
+    public ArrayList<Block> getBC() {
+        return BC;
     }
 }
