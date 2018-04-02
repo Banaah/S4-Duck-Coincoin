7b6b616fdd72315e7842fe4e8517474aaacdd148
Nyktalop <36138631+Nyktalop@users.noreply.github.com>
2018-04-03 01:44:16 +0200

    Debut de merkleRootIt()
    Pour l'instant ça marche pas, mais j'commit pour OUI

--- src/C/main.c
+++ src/C/main.c
@@ -1,13 +1,41 @@
 #define INPUT_LENGTH 20
 
 #include "blockchain.h"
+#include "merkleroot.h"
 
 int main(){
 	char input[INPUT_LENGTH];
 	int indice;
 
-	printf("Bienvenue dans l'implementation du DuckCoinCoin par la Team Belette !\n\t\tQuentin Nouvel\tFrancois Gaits\tLeo Guimas\n");
+	char *tab[] = {"Source-Destination:88",
+				   "Source-Destination:55",
+				   "Source-Destination:4",
+				   "Source-Destination:94",
+				   "Source-Destination:63",
+				   "Source-Destination:68",
+				   "Source-Destination:21",
+				   "Source-Destination:23",
+				   "Source-Destination:53",
+				   "Source-Destination:15",
+				   "Source-Destination:9",
+				   "Source-Destination:96",
+				   "Source-Destination:11",
+				   "Source-Destination:78",
+				   "Source-Destination:17",
+				   "Source-Destination:37",
+				   "Source-Destination:35",
+				   "Source-Destination:22",
+				   "Source-Destination:93",
+				   "Source-Destination:17",
+				   "Source-Destination:69"};
 
+	char *res = merkleRootIt(tab,21);
+	printf("OUI : %s\n",res);
+
+	char *res2 = getMerkleRoot(tab,21);
+	printf("OUI : %s\n",res2);
+
+	printf("Bienvenue dans l'implementation du DuckCoinCoin par la Team Belette !\n\t\tQuentin Nouvel\tFrancois Gaits\tLeo Guimas\n");
 	printf("Generer une blockchain ? [o/n]\n");
 
 	scanf("%s",input);
